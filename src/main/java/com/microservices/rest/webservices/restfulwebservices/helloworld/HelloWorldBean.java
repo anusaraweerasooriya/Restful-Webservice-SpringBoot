6a75b28b92120ac19c56c725e8af4b0d9556c78b
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-12-02 22:13:14 +0530

    Initial Commit

---
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,26 @@
+package com.microservices.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+
+}
