5034662727cdbff79a40e938a3b998395a1c0b82
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-12-02 22:26:37 +0530

    Implemented UserResource

---
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/user/UserResource.java
@@ -0,0 +1,21 @@
+package com.microservices.rest.webservices.restfulwebservices.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return  service.findAll();
+    }
+}
