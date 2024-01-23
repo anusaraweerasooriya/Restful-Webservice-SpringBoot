762400fc887962f07a9cf9d1d731fb2cf8bcb2b3
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2024-01-24 02:40:43 +0530

    Implementing Static Filtering for REST API and ignoring fields

--- src/main/java/com/microservices/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/microservices/rest/webservices/restfulwebservices/user/User.java
@@ -5,13 +5,17 @@ import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
     private Integer id;
 
-    @Size(min=2, message = "Name should have at least 2 characters")
+    @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
